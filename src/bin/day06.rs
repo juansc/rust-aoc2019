f70869a5113376b6fad34bd243af17e254827868
Juan <juanscarrillo23@gmail.com>
2021-05-15 20:35:46 -0700

    cleaned up part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,18 +1,16 @@
 use std::collections::HashMap;
 
 use aoc2019::util;
-use std::borrow::Borrow;
 use std::cmp::min;
 
 fn main() {
-    let lines = util::lines_from_file("./input/day04.txt");
+    let lines = util::lines_from_file("./input/day06.txt");
 
     println!("Part 1 Solution: {}", part1(&lines));
     println!("Part 2 Solution: {}", part2(&lines));
 }
 
-fn part1(lines: &[String]) -> u32 {
-    // Create a mapping from satellite -> primary.
+fn get_orbits(lines: &[String]) -> HashMap<String, String> {
     let mut orbits = HashMap::new();
     lines
         .iter()
@@ -20,6 +18,12 @@ fn part1(lines: &[String]) -> u32 {
         .for_each(|o| {
             orbits.insert(o[1].to_string(), o[0].to_string());
         });
+    orbits
+}
+
+fn part1(lines: &[String]) -> u32 {
+    // Create a mapping from satellite -> primary.
+    let orbits = get_orbits(lines);
 
     // For every body, repeatedly go down the primary until you get to the COM.
     let mut counter = 0;
@@ -33,46 +37,32 @@ fn part1(lines: &[String]) -> u32 {
     counter
 }
 
+fn dist_to_com(body: String, orbits: &HashMap<String, String>) -> HashMap<String, u32> {
+    let mut distances = HashMap::new();
+    let mut body = orbits.get(&body).unwrap().clone();
+    let mut counter = 0u32;
+    while body != "COM" {
+        counter += 1;
+        body = orbits.get(&body).unwrap().clone();
+        distances.insert(body.clone(), counter);
+    }
+    distances
+}
+
 fn part2(lines: &[String]) -> u32 {
     // Create a mapping from satellite -> primary.
-    let mut orbits = HashMap::new();
-    lines
-        .iter()
-        .map(|l| l.split(')').collect::<Vec<&str>>())
-        .for_each(|o| {
-            orbits.insert(o[1].to_string(), o[0].to_string());
-        });
+    let orbits = get_orbits(lines);
 
     // Create a map indicating how many steps it took to go from YOU to the given body.
     // Create a map indicating how many steps it took to go from SAN to a given body.
     // Find the body that they have in common with the smallest commulative distance.
-    let mut dist_to_you = HashMap::new();
-    let mut body = orbits.get(&"YOU".to_string()).unwrap().clone();
-    let mut counter = 0u32;
-    while body != "COM" {
-        counter += 1;
-        body = orbits.get(&body).unwrap().clone();
-        dist_to_you.insert(body.clone(), counter);
-    }
+    let dist_to_you = dist_to_com("YOU".to_string(), &orbits);
+    let dist_to_santa = dist_to_com("SAN".to_string(), &orbits);
 
-    let mut dist_to_santa = HashMap::new();
-    let mut body = orbits.get(&"SAN".to_string()).unwrap().clone();
-    let mut counter = 0u32;
-    while body != "COM" {
-        counter += 1;
-        body = orbits.get(&body).unwrap().clone();
-        dist_to_santa.insert(body.clone(), counter);
-    }
     let mut min_distance = u32::MAX;
-    for (body, dist) in dist_to_you.iter() {
-        match dist_to_santa.get(body) {
-            Some(d) => {
-                let dd = d + dist;
-                if dd < min_distance {
-                    min_distance = dd;
-                }
-            }
-            _ => {}
+    for (body, d1) in dist_to_you.iter() {
+        if let Some(d2) = dist_to_santa.get(body) {
+            min_distance = min(d2 + d1, min_distance);
         }
     }
     min_distance
