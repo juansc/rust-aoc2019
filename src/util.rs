562d07db1d7a07330d9d65094a79b9df0d19ec24
Juan <juanscarrillo23@gmail.com>
2021-07-30 17:44:13 -0700

    added permutation function, but it needs to become a lazy iterator

--- src/util.rs
+++ src/util.rs
@@ -24,3 +24,69 @@ pub fn read_int_code_memory(filename: impl AsRef<Path>) -> Vec<i32> {
         .filter_map(Result::ok)
         .collect()
 }
+
+struct Permutations {
+    current: Vec<i32>,
+    ind: u16,
+}
+
+pub fn perms(k: usize, input: &mut Vec<i32>) -> Vec<Vec<i32>> {
+    let mut out: Vec<Vec<i32>> = Vec::new();
+    if k == 1 {
+        return vec![input.clone()];
+    }
+    let out_perms = perms(k - 1, input);
+    for e in out_perms {
+        out.push(e.clone());
+    }
+    for i in 0..k - 1 {
+        if k % 2 == 0 {
+            input.swap(i, k - 1);
+        } else {
+            input.swap(0, k - 1);
+        }
+        let out_perms = perms(k - 1, input);
+        for e in out_perms {
+            out.push(e.clone());
+        }
+    }
+    out
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::util::perms;
+
+    #[test]
+    fn test_single_elem_permuration() {
+        let mut elements = vec![1i32];
+        assert_eq!(vec![vec![1i32]], perms(elements.len(), &mut elements));
+    }
+
+    #[test]
+    fn test_double_elem_permuration() {
+        let mut elements = vec![0i32, 1i32];
+
+        assert_eq!(
+            vec![vec![0i32, 1i32], vec![1i32, 0i32]],
+            perms(elements.len(), &mut elements)
+        );
+    }
+
+    #[test]
+    fn test_three_elem_permuration() {
+        let mut elements = vec![0i32, 1i32, 2i32];
+
+        assert_eq!(
+            vec![
+                vec![0i32, 1i32, 2i32],
+                vec![1i32, 0i32, 2i32],
+                vec![2i32, 0i32, 1i32],
+                vec![0i32, 2i32, 1i32],
+                vec![1i32, 2i32, 0i32],
+                vec![2i32, 1i32, 0i32],
+            ],
+            perms(elements.len(), &mut elements)
+        );
+    }
+}
