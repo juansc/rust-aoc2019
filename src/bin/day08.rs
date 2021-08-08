1e8724297eb9897cf4eda15d54c9f1746e20856c
Juan <juanscarrillo23@gmail.com>
2021-08-07 19:03:39 -0700

    rewrote as iterator, may not actually be better XD

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -8,22 +8,25 @@ fn main() {
 
 fn part1(line: &[String], height: usize, width: usize) -> usize {
     let image = Image::new(line, height, width);
-    let mut layer_with_least_zeroes = 0;
-    let mut min_zeroes = usize::MAX;
-    for i in 0..image.layers {
-        let zeroes = image.digits_in_layer(i, 0);
-        if zeroes < min_zeroes {
-            min_zeroes = zeroes;
-            layer_with_least_zeroes = i;
-        }
-    }
+    // Go through all the layers. The accumulator here is (index, value), and we keep the minimum
+    // value and the layer it happened. It's may not be faster, but it was an interesting way
+    // to write it as an iterator
+    let layer_with_least_zeroes = (0..image.layers)
+        .fold((0, usize::MAX), |acc, i| {
+            let val = image.digits_in_layer(i, 0);
+            if val < acc.1 {
+                (i, val)
+            } else {
+                acc
+            }
+        })
+        .0;
     image.digits_in_layer(layer_with_least_zeroes, 1)
         * image.digits_in_layer(layer_with_least_zeroes, 2)
 }
 
 fn part2(line: &[String], height: usize, width: usize) -> String {
-    let image = Image::new(line, height, width);
-    image.image_string()
+    Image::new(line, height, width).image_string()
 }
 
 struct Image {
@@ -64,13 +67,10 @@ impl Image {
     }
 
     fn digits_in_layer(&self, layer: usize, digit: u8) -> usize {
-        let mut n = 0;
-        for i in self.layer_size * layer..self.layer_size * (layer + 1) {
-            if *self.pixels.get(i).unwrap() == digit {
-                n += 1;
-            }
-        }
-        n
+        self.pixels[self.layer_size * layer..self.layer_size * (layer + 1)]
+            .iter()
+            .filter(|n| **n == digit)
+            .count()
     }
 
     fn pixel(&self, layer: usize, x: usize, y: usize) -> Option<u8> {
@@ -101,8 +101,7 @@ impl Image {
 
     fn color_for_pixel(&self, x: usize, y: usize) -> Color {
         for layer in 0..self.layers {
-            let color = self.pixel(layer, x, y).unwrap();
-            match color {
+            match self.pixel(layer, x, y).unwrap() {
                 0 => return Color::Black,
                 1 => return Color::White,
                 _ => continue,
@@ -124,11 +123,6 @@ enum Color {
 mod tests {
     use crate::util;
     use crate::{part1, part2};
-
-    #[test]
-    fn test_fuel_for_mass() {
-        assert_eq!(1, 1);
-    }
 
     #[test]
     fn test_part1_simple() {
